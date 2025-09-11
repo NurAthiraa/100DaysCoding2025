0202808c99c1720ffe12d362216da9f3bfce2c70
NurAthiraa <athiranur64@gmail.com>
2025-09-11 02:25:50 -0700

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,36 @@
+public class Day4 {
+    public static void main(String[] args) {
+        // 1. \n (New Line)
+        System.out.println("1. Menggunakan New Line");
+        System.out.print("Aktivitas Pagi: Bangun Tidur\nAktivitas Malam: Tidur Lagi\n\n");
+
+        // 2. \t (Horizontal Tab)
+        System.out.println("2. Menggunakan Horizontal Tab");
+        System.out.println("Kebiasaan\t: Tidur Siang");
+        System.out.println("Durasi\t\t: 2 Jam\n");
+
+        // 3. \\ (Backslash)
+        System.out.println("3. Menggunakan Backslash");
+        System.out.println("Folder\\Harian\\Tidur\\Nyenyak\n");
+
+        // 4. \" (Double Quote)
+        System.out.println("4. Menggunakan Double Quote");
+        System.out.println("Dia berkata: \"Tidur itu penting untuk kesehatan!\"\n");
+
+        // 5. \' (Single Quote)
+        System.out.println("5. Menggunakan Single Quote");
+        System.out.println("Motivasi: \'Tidur cukup biar segar\'\n");
+
+        // 6. \b (Backspace)
+        System.out.println("6. Menggunakan Backspace");
+        System.out.println("Tidurrr\b\n");
+
+        // 7. \r (Carriage Return)
+        System.out.println("7. Menggunakan Carriage Return");
+        System.out.print("Lagi begadang... \rTidur\n");
+
+        // 8. Gabungan
+        System.out.println("8. Menggunakan Gabungan");
+        System.out.println("Catatan ada di C:\\Users\\Tidur\\Diary");
+    }
+}
