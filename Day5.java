680ac65a2721ca95b876927071ce75d192381c54
NurAthiraa <athiranur64@gmail.com>
2025-09-12 03:31:06 -0700

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class Day5 {
+    public static void main(String[] args) {
+        // 4  tipe data bilangan bulat
+        byte a = 10;
+        short b = 200;
+        int c = 30000;
+        long d = 400000L;
+
+        // cetak semua variabel
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
