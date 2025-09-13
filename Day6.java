4bf70812aba2cc52a716d4f531b0b16c4863c3c9
NurAthiraa <athiranur64@gmail.com>
2025-09-13 18:16:21 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class Day6 {
+        public static void main(String[] args) {
+            // Tipe data float (single-precision)
+            float xFloat = 12.345f;
+
+            // Tipe data double (double-precision)
+            double xDouble = 12.3456789012345;
+
+            // Menampilkan hasil
+            System.out.println("Nilai float: " + xFloat);
+            System.out.println("Nilai double: " + xDouble);
+        }
+    }
