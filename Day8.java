35740d418d83f2f5b301735332cebfe7ffaf3402
NurAthiraa <athiranur64@gmail.com>
2025-09-15 04:23:36 -0700

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,14 @@
+public class Day8 {
+        public static void main(String[] args) {
+            String alasanISC = "Saya masuk ISC karena ingin mencari pengalaman baru, memperluas relasi, dan mengisi waktu dengan kegiatan positif.";
+            String alasanProdi = "Saya pilih Informatika karena sejak SMK mengambil jurusan TKJ, jadi ingin melanjutkan dan memperdalam ilmu di bidang komputer.";
+            String salahPilih = "Sejauh ini saya merasa tidak salah jurusan, walaupun kadang tugasnya berat tapi masih sesuai minat.";
+            String kendala = "Kendala saya selama di prodi ini terutama di PBO dan DDP adalah sulit memahami sintaks baru dan butuh waktu lebih untuk latihan ngoding.";
+
+            System.out.println("1. Alasan masuk ISC: " + alasanISC);
+            System.out.println("2. Alasan memilih prodi Informatika: " + alasanProdi);
+            System.out.println("3. Apakah salah pilih jurusan: " + salahPilih);
+            System.out.println("4. Kendala di prodi: " + kendala);
+        }
+    }
+
