bdfcf0e24ffbaf2a50cc983dc23765844c4e72fa
NurAthiraa <athiranur64@gmail.com>
2025-09-17 06:10:32 -0700

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,6 @@
+public class Day10 {
+    public static void main(String[] args) {
+       final String NAMA = "Ayam Geprek";
+        System.out.println("Cinta sejati saya selamanya adalah: " + NAMA);
+    }
+}
