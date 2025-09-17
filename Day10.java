508b2b68c6520bf26c36d6f50d2d7363899cf11e
NurAthiraa <athiranur64@gmail.com>
2025-09-17 06:32:48 -0700

    Day10.java

--- Day10.java
+++ Day10.java
@@ -1,6 +1,10 @@
 public class Day10 {
     public static void main(String[] args) {
-       final String NAMA = "Ayam Geprek";
-        System.out.println("Cinta sejati saya selamanya adalah: " + NAMA);
+        final int HARGA_AYAM_GEPREK = 20000;
+        int jumlahPorsi = 3;
+        int total = HARGA_AYAM_GEPREK * jumlahPorsi;
+
+        System.out.println("Beli " + jumlahPorsi + " porsi ayam geprek: Rp" + total);
+        System.out.println("Dompet nangis! Menapa bisa etorr ");
     }
 }
