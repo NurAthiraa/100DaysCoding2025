59873be0bf78d99f71aa6eecf10a1e5b5b903184
NurAthiraa <athiranur64@gmail.com>
2025-09-22 06:49:23 -0700

    Day15.java

--- Day15.java
+++ Day15.java
@@ -1,28 +1,18 @@
-import java.util.Scanner;
-    public class Day15{
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+public class Day15 {
+    public static void main(String[] args) {
+        String str1 = "10";
+        String str2 = "5";
 
-            // input angka dalam bentuk String
-            System.out.print("Masukkan angka pertama (String): ");
-            String str1 = sc.nextLine();
-            System.out.print("Masukkan angka kedua (String): ");
-            String str2 = sc.nextLine();
+        String gabungan = str1 + str2;
+        System.out.println("Sebelum parse (String digabung): " + gabungan);
 
-            // operasi sebelum parse
-            String gabungan = str1 + str2;
-            System.out.println("\nSebelum parse (String digabung): " + gabungan);
+        int angka1 = Integer.parseInt(str1);
+        int angka2 = Integer.parseInt(str2);
 
-            // parse String ke int
-            int angka1 = Integer.parseInt(str1);
-            int angka2 = Integer.parseInt(str2);
+        int jumlah = angka1 + angka2;
+        int kurang = angka1 - angka2;
 
-            // operasi setelah parse (penjumlahan dan pengurangan)
-            int jumlah = angka1 + angka2;
-            int kurang = angka1 - angka2;
-
-            System.out.println("Setelah parse (penjumlahan): " + jumlah);
-            System.out.println("Setelah parse (pengurangan): " + kurang);
-        }
+        System.out.println("Setelah parse (penjumlahan): " + jumlah);
+        System.out.println("Setelah parse (pengurangan): " + kurang);
     }
-
+}
