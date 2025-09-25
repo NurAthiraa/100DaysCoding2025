d77d1443c92cdac1a815f75ab80710c9b1667fd6
NurAthiraa <athiranur64@gmail.com>
2025-09-25 05:08:02 -0700

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,11 @@
+public class HitungBelanja {
+    public static void main(String[] args) {
+        int hargaBarang = 150000;
+        double pajak = 16500.50;
+
+        double totalBayar;
+        totalBayar = hargaBarang + pajak;
+
+        System.out.println("Total yang harus dibayar: " + totalBayar);
+    }
+}
