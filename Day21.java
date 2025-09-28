9ee356e5ec0e230af35ca4b6b62e099ef7b1bff0
NurAthiraa <athiranur64@gmail.com>
2025-09-28 22:01:27 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,18 @@
+public class TukarNilai {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sebelum ditukar:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("\nSetelah ditukar:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
