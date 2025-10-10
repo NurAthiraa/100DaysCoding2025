160c77d08c29c6ae49ec576fd3d35355de4b7ec8
NurAthiraa <athiranur64@gmail.com>
2025-10-10 19:57:31 +0800

    Day33.java

--- Day33.java
+++ Day33.java
@@ -4,7 +4,7 @@ public class Day33 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== Cek Kelayakan Masuk Area VIP ===");
+        System.out.println("=== Program Cek Kelayakan Masuk Area VIP ===");
         System.out.print("Masukkan umur kamu: ");
         int umur = sc.nextInt();
 
@@ -13,11 +13,8 @@ public class Day33 {
 
         boolean bolehMasuk = (umur >= 18) && vip;
 
-        if (!bolehMasuk) {
-            System.out.println("Maaf, kamu belum memenuhi syarat untuk masuk ke area VIP.");
-        } else {
-            System.out.println("Selamat datang di area VIP!");
-        }
+        System.out.println("Status boleh masuk: " + bolehMasuk);
+        System.out.println("Kebalikannya (hasil dari !bolehMasuk): " + !bolehMasuk);
 
         sc.close();
     }
