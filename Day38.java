23d5652fa8450ba795f4411f0d3eb91056e57f2f
NurAthiraa <athiranur64@gmail.com>
2025-10-15 22:38:37 +0800

    Day38.java

--- Day38.java
+++ Day38.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 public class Day38 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        // Meminta pengguna untuk memasukkan angka
         System.out.print("Masukkan angka: ");
         int angka = sc.nextInt();
         
-        // Menentukan apakah angka positif, negatif, atau nol
         if (angka > 0) {
             System.out.println(angka + " adalah bilangan positif");
         } else if (angka < 0) {
@@ -17,7 +14,6 @@ public class Day38 {
             System.out.println("Bilangan tersebut adalah nol");
         }
         
-        // Menutup scanner
         sc.close();
     }
 }
