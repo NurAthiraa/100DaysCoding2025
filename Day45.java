913cd5d1252dc5d1e0f512f3198a449c65015858
NurAthiraa <athiranur64@gmail.com>
2025-10-22 19:15:49 +0800

    Day45.java

--- Day45.java
+++ Day45.java
@@ -1,42 +1,26 @@
 import java.util.Scanner;
 
-public class Day45 {
+public class Day45IfElse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan angka (1-7): ");
         int angka = sc.nextInt();
 
-        String hari;
-
-        switch (angka) {
-            case 1:
-                hari = "Senin";
-                break;
-            case 2:
-                hari = "Selasa";
-                break;
-            case 3:
-                hari = "Rabu";
-                break;
-            case 4:
-                hari = "Kamis";
-                break;
-            case 5:
-                hari = "Jumat";
-                break;
-            case 6:
-                hari = "Sabtu";
-                break;
-            case 7:
-                hari = "Minggu";
-                break;
-            default:
-                hari = null;
-        }
-
-        if (hari != null) {
-            System.out.println("Sekarang adalah hari " + hari);
+        if (angka == 1) {
+            System.out.println("Sekarang adalah hari Senin");
+        } else if (angka == 2) {
+            System.out.println("Sekarang adalah hari Selasa");
+        } else if (angka == 3) {
+            System.out.println("Sekarang adalah hari Rabu");
+        } else if (angka == 4) {
+            System.out.println("Sekarang adalah hari Kamis");
+        } else if (angka == 5) {
+            System.out.println("Sekarang adalah hari Jumat");
+        } else if (angka == 6) {
+            System.out.println("Sekarang adalah hari Sabtu");
+        } else if (angka == 7) {
+            System.out.println("Sekarang adalah hari Minggu");
         } else {
             System.out.println("Jumlah hari hanya 7");
         }
