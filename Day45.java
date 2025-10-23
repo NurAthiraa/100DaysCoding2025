b2add6d0c2113e8aed1da8becae92785d873cd23
NurAthiraa <athiranur64@gmail.com>
2025-10-23 21:46:00 +0800

    Day45.java

--- Day45.java
+++ Day45.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
-public class Day45IfElse {
+public class Day46 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan angka (1-7): ");
         int angka = sc.nextInt();
 
-        if (angka == 1) {
-            System.out.println("Sekarang adalah hari Senin");
-        } else if (angka == 2) {
-            System.out.println("Sekarang adalah hari Selasa");
-        } else if (angka == 3) {
-            System.out.println("Sekarang adalah hari Rabu");
-        } else if (angka == 4) {
-            System.out.println("Sekarang adalah hari Kamis");
-        } else if (angka == 5) {
-            System.out.println("Sekarang adalah hari Jumat");
-        } else if (angka == 6) {
-            System.out.println("Sekarang adalah hari Sabtu");
-        } else if (angka == 7) {
-            System.out.println("Sekarang adalah hari Minggu");
-        } else {
-            System.out.println("Jumlah hari hanya 7");
+        switch (angka) {
+            case 1:
+                System.out.println("Sekarang adalah hari Senin");
+                break;
+            case 2:
+                System.out.println("Sekarang adalah hari Selasa");
+                break;
+            case 3:
+                System.out.println("Sekarang adalah hari Rabu");
+                break;
+            case 4:
+                System.out.println("Sekarang adalah hari Kamis");
+                break;
+            case 5:
+                System.out.println("Sekarang adalah hari Jumat");
+                break;
+            case 6:
+                System.out.println("Sekarang adalah hari Sabtu");
+                break;
+            case 7:
+                System.out.println("Sekarang adalah hari Minggu");
+                break;
+            default:
+                System.out.println("Jumlah hari hanya 7");
         }
 
         sc.close();
