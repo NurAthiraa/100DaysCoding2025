e27bbf37c068a1bab8c0b96c1ef0c7d022580b2f
NurAthiraa <athiranur64@gmail.com>
2025-10-25 06:53:49 -0700

    Day48.java

--- Day48.java
+++ Day48.java
@@ -1,43 +1,41 @@
 import java.util.Scanner;
 
-public class Day48 {
+public class day48 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         boolean valid = true;
-
-        System.out.print("Masukkan angka pertama: ");
-        String input1 = sc.next();
-        System.out.print("Masukkan operator (+, -, *, /): ");
-        String operator = sc.next();
-        System.out.print("Masukkan angka kedua: ");
-        String input2 = sc.next();
 
         double angka1 = 0;
         double angka2 = 0;
+        char operator = ' ';
+        double hasil = 0;
 
         try {
-            angka1 = Double.parseDouble(input1);
-            angka2 = Double.parseDouble(input2);
-        } catch (NumberFormatException e) {
+            System.out.print("Masukkan angka pertama: ");
+            angka1 = sc.nextDouble();
+
+            System.out.print("Masukkan operator (+, -, *, /): ");
+            operator = sc.next().charAt(0);
+
+            System.out.print("Masukkan angka kedua: ");
+            angka2 = sc.nextDouble();
+        } catch (Exception e) {
             System.out.println("Error: Input angka tidak valid!");
             valid = false;
         }
 
         if (valid) {
-            double hasil = 0;
-
             switch (operator) {
-                case "+":
+                case '+':
                     hasil = angka1 + angka2;
                     break;
-                case "-":
+                case '-':
                     hasil = angka1 - angka2;
                     break;
-                case "*":
+                case '*':
                     hasil = angka1 * angka2;
                     break;
-                case "/":
+                case '/':
                     if (angka2 == 0) {
                         System.out.println("Error: Pembagian dengan nol tidak diperbolehkan!");
                         valid = false;
@@ -49,14 +47,16 @@ public class Day48 {
                     System.out.println("Operator tidak dikenali. Gunakan +, -, *, atau /.");
                     valid = false;
             }
+        }
 
-            if (valid) {
-                // Tentukan apakah hasilnya int atau double
-                boolean angka1Bulat = angka1 % 1 == 0;
-                boolean angka2Bulat = angka2 % 1 == 0;
-                boolean hasilBulat = hasil % 1 == 0;
+        if (valid) {
+            boolean inputDesimal = (angka1 % 1 != 0) || (angka2 % 1 != 0);
+            boolean hasilBulat = hasil % 1 == 0;
 
-                if (angka1Bulat && angka2Bulat && hasilBulat) {
+            if (inputDesimal) {
+                System.out.println("Hasil: " + hasil);
+            } else {
+                if (hasilBulat) {
                     System.out.println("Hasil: " + (int) hasil);
                 } else {
                     System.out.println("Hasil: " + hasil);
