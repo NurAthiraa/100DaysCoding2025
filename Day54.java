82e3bcb087e8e84de4b00fd525ac12270f0326b8
NurAthiraa <athiranur64@gmail.com>
2025-10-31 22:03:52 +0800

    Day54.java

--- Day54.java
+++ Day54.java
@@ -1,1 +1,17 @@
+public class Day54 {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("     PROGRAM KEYWORD CONTINUE (Day54)    ");
+        System.out.println("========================================\n");
+
+        System.out.println("Menampilkan angka 1 sampai 20 (tanpa angka genap):");
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
 
