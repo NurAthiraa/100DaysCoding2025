359282a7b5d3c55286cb570cad20cd288f11322d
NurAthiraa <athiranur64@gmail.com>
2025-11-08 20:11:30 +0800

    Day62.java

---
+++ Day62.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Day62 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan batas angka (N): ");
+        int n = sc.nextInt();
+
+        int total = 0;
+
+        System.out.print("Proses: ");
+
+        // for loop untuk menjumlahkan
+        for (int i = 1; i <= n; i++) {
+            total += i; // proses penjumlahan
+            System.out.print(i);
+
+            if (i < n) {
+                System.out.print(" + ");
+            }
+        }
+
+        System.out.println(" = " + total);
+
+        System.out.println("Total penjumlahan dari 1 sampai " + n + " adalah: " + total);
+    }
+}
