48708cdc13453c055cae52fc80b9d2ce9440de52
NurAthiraa <athiranur64@gmail.com>
2025-11-13 18:57:09 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Pola Garis Horizontal ===");
+        System.out.print("Masukkan jumlah N: ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print("* ");
+        }
+    }
+}
