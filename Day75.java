0834cec6bbedcc41ae303d4f1e5c9e22aa0a39c1
NurAthiraa <athiranur64@gmail.com>
2025-11-21 19:25:33 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,13 @@
+public class Day75 {
+    public static void main(String[] args) {
+
+        String teks = "Pemrograman Java";
+
+        System.out.println("=== Manipulasi String Dasar ===");
+        System.out.println("Teks Asli: " + teks);
+        System.out.println("Panjang karakter: " + teks.length());
+        System.out.println("Huruf Besar: " + teks.toUpperCase());
+        System.out.println("Huruf Kecil: " + teks.toLowerCase());
+        System.out.println("Cek Teks Asli Lagi: " + teks);
+    }
+}
