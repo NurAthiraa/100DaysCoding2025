d6320fd6b04fb9e3e59c941ec7a6295d44054e63
NurAthiraa <athiranur64@gmail.com>
2025-12-01 19:20:49 +0800

    Day85.java

--- Day85.java
+++ Day85.java
@@ -1,1 +1,22 @@
+public class Day83StringMax {
+
+    public static void main(String[] args) {
+
+        String[] namaSiswa = {"Ai", "Zerlina", "Aika", "Kayla", "Yanto"};
+
+        String namaPalingAkhir = namaSiswa[0];
+
+        System.out.println("=== Daftar Nama ===");
+        for (int i = 0; i < namaSiswa.length; i++) {
+            System.out.println("== " + namaSiswa[i] + " ==");
+
+            if (namaSiswa[i].compareTo(namaPalingAkhir) > 0) {
+                namaPalingAkhir = namaSiswa[i];
+            }
+        }
+
+        System.out.println("===========================");
+        System.out.println("== Siswa Absen Terakhir (Max): " + namaPalingAkhir + " ==");
+    }
+}
 
