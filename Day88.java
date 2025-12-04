454adabfe0f3541c2d3d833f4325549d4d0042d6
NurAthiraa <athiranur64@gmail.com>
2025-12-04 05:20:55 -0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,10 @@
+public class Day88 {
+    public static void main(String[] args) {
+
+        String[] nama = {"Ultraman Tiga", "Ultraman Dyna", "Ultraman Gaia", "Ultraman Cosmos", "Ultraman Mebius"};
+
+        for (String x : nama) {
+            System.out.println("Isi array: " + x);
+        }
+    }
+}
