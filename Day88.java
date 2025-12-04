70825332ead8f7939913ef0cba05b298d4f67a55
NurAthiraa <athiranur64@gmail.com>
2025-12-04 05:27:13 -0800

    Day88.java

--- Day88.java
+++ Day88.java
@@ -1,7 +1,7 @@
 public class Day88 {
     public static void main(String[] args) {
 
-        String[] nama = {"Ultraman Tiga", "Ultraman Dyna", "Ultraman Gaia", "Ultraman Cosmos", "Ultraman Mebius"};
+        String[] nama = {"Ultraman Tiga", "Ultraman Dyna", "Ultraman Gaia", "Ultraman Cosmos", "Ultraman Mebius", "Ultraman Max", "Ultraman Nexus", "Ultraman Ginga", "Ultraman Victory", "Ultraman X", "Ultraman Orb", "Ultraman Geed", "Ultraman Z", "Ultraman Trigger", "Ultraman Decker"};
 
         for (String x : nama) {
             System.out.println("Isi array: " + x);
