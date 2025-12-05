dff379cf46cb4b5c813c455a9347950dd1285176
NurAthiraa <athiranur64@gmail.com>
2025-12-05 20:53:15 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,10 @@
+public class Day89 {
+
+    public static void sapaDefault() {
+        System.out.println("Halo, selamat belajar Java!");
+    }
+
+    public static void main(String[] args) {
+        sapaDefault();
+    }
+}
