75f61c5beffe04984bcadd6b0fae115c3c27359c
NurAthiraa <athiranur64@gmail.com>
2025-12-08 20:41:30 +0800

    Day92.java

---
+++ Day92.java
@@ -0,0 +1,9 @@
+public class Day92 {
+    static int hitungGaji(int jam) {
+        return jam * 50000;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Gaji: Rp " + hitungGaji(8));
+    }
+}
