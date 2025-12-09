9904ed3c07dc4870a380ce48701b7dc2409b56d6
NurAthiraa <athiranur64@gmail.com>
2025-12-09 10:20:30 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,18 @@
+public class Day93 {
+    public static void main(String[] args) {
+
+        double angka1 = 7.2;
+        double angka2 = 7.6;
+
+        System.out.println("=== Contoh Method ceil(), floor(), dan round() ===");
+        System.out.println("Angka: " + angka1);
+        System.out.println("ceil  : " + Math.ceil(angka1));
+        System.out.println("floor : " + Math.floor(angka1));
+        System.out.println("round : " + Math.round(angka1));
+
+        System.out.println("\nAngka: " + angka2);
+        System.out.println("ceil  : " + Math.ceil(angka2));
+        System.out.println("floor : " + Math.floor(angka2));
+        System.out.println("round : " + Math.round(angka2));
+    }
+}
